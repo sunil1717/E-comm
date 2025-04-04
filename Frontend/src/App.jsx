1091e15c5737868df6d1f3d893fa73163cb4597d
sunil1717 <sunilsahoo745757@gmail.com>
2025-04-05 02:11:31 +0530

    5th

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home';
+import Home from './Pages/Home'
 
 import About from './Pages/About'
 import Collection from './Pages/Collection'
