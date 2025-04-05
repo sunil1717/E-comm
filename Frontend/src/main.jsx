94a3923473c41b84bf478514b41e6854a73e910d
sunil1717 <sunilsahoo745757@gmail.com>
2025-04-05 14:19:07 +0530

    11

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react'
+
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
-import ShopcontextProvider from './Context/Shopcontext';
+import ShopcontextProvider from './Context/shopContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
       <ShopcontextProvider> 
-
-      <App />
+         <App />
       </ShopcontextProvider> 
   </BrowserRouter>,
 )
